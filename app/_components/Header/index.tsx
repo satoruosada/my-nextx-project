de306381e4bd768c3a85311e883700ad598e8b41
osada <osada-s@aap.co.jp>
2026-02-03 14:42:16 +0900

    メンバーリストの組み込み

--- app/_components/Header/index.tsx
+++ app/_components/Header/index.tsx
@@ -1,19 +1,23 @@
 import Image from "next/image";
+import Link from "next/link";
 import styles from './index.module.css';
+import Menu from '../Menu';
+
 
 export default function Header() {
     return (
         <header className={styles.header}>
-            <a href="/" className={styles.logoLink}>
-                <Image
-                    src="/logo.svg"
-                    alt="SIMPLE"
-                    className={styles.logo}
-                    width={348}
-                    height={133}
-                    priority
-                />
-            </a>
+            <Link href="/" className={styles.logoLink}>
+            <Image
+                src="/logo.svg"
+                alt="SIMPLE"
+                className={styles.logo}
+                width={348}
+                height={133}
+                priority
+            />
+            </Link>
+            <Menu />
         </header>
     );
 }
